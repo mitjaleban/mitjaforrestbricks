0f3f3acc1396d2efca751708b08d388c8d683033
mitjaleban <81330753+mitjaleban@users.noreply.github.com>
2021-03-31 11:03:58 +0200

    Update js.js

--- js.js
+++ js.js
@@ -73,14 +73,14 @@ function collisionDetection() {
 function drawBall() {
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-  ctx.fillStyle ="#ffff00";
+  ctx.fillStyle ="#999966";
   ctx.fill();
   ctx.closePath();
 }
 function drawPaddle() {
   ctx.beginPath();
   ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-  ctx.fillStyle ="#ffff00";
+  ctx.fillStyle ="#999966";
   ctx.fill();
   ctx.closePath();
 }
